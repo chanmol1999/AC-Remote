568d28194118470030fccb960f40d9430d9b49d5
chanmol1999 <chanmol1999@gmail.com>
2019-05-23 20:57:53 +0530

    Add check connection

--- AC--Remote/app/src/main/java/com/example/ac_remote/MainActivity.java
+++ AC--Remote/app/src/main/java/com/example/ac_remote/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ac_remote;
 
+import android.content.Context;
 import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,103 +33,111 @@ public class MainActivity extends AppCompatActivity {
         power = findViewById( R.id.power );
         plus = findViewById( R.id.plus_button );
         minus = findViewById( R.id.minus_button );
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        final DatabaseReference swingd = database.getReference( "swing" );
-        final DatabaseReference tempd = database.getReference( "temp" );
-        final DatabaseReference powerd = database.getReference( "power" );
-        swingd.addValueEventListener( new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                swingvalue = (String) dataSnapshot.getValue();
-                if (swingvalue.equals( "1" )) {
-                    swing.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
-                } else {
-                    swing.setTextColor( getResources().getColor( android.R.color.black ) );
+        if(isNetworkAvailable()) {
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            final DatabaseReference swingd = database.getReference( "swing" );
+            final DatabaseReference tempd = database.getReference( "temp" );
+            final DatabaseReference powerd = database.getReference( "power" );
+            swingd.addValueEventListener( new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    swingvalue = (String) dataSnapshot.getValue();
+                    if (swingvalue.equals( "1" )) {
+                        swing.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
+                    } else {
+                        swing.setTextColor( getResources().getColor( android.R.color.black ) );
+                    }
                 }
-            }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        } );
-
-        swing.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (swingvalue.equals( "1" )) {
-                    swingd.setValue( "0" );
-                    swing.setTextColor( getResources().getColor( android.R.color.black ) );
-                } else {
-                    swingd.setValue( "1" );
-                    swing.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
-                }
-            }
-        } );
-        powerd.addValueEventListener( new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                powervalue = (String) dataSnapshot.getValue();
-                if (powervalue.equals( "1" )) {
-                    power.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
-
-                } else {
-                    power.setTextColor( getResources().getColor( android.R.color.black ) );
-                }
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        } );
-
-        power.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (powervalue.equals( "1" )) {
-                    power.setTextColor( getResources().getColor( android.R.color.black ) );
-                    powerd.setValue( "0" );
-                } else {
-                    powerd.setValue( "1" );
-                    power.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
                 }
-            }
-        } );
-        tempd.addValueEventListener( new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                tempvalue = (String) dataSnapshot.getValue();
-                temperature.setText( tempvalue + " °C" );
-            }
+            } );
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+            swing.setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (swingvalue.equals( "1" )) {
+                        swingd.setValue( "0" );
+                        swing.setTextColor( getResources().getColor( android.R.color.black ) );
+                    } else {
+                        swingd.setValue( "1" );
+                        swing.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
+                    }
+                }
+            } );
+            powerd.addValueEventListener( new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    powervalue = (String) dataSnapshot.getValue();
+                    if (powervalue.equals( "1" )) {
+                        power.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
 
-            }
-        } );
-        plus.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int temp = Integer.parseInt( tempvalue );
-                if (temp < 40)
-                    ++temp;
-                tempd.setValue( "" + temp );
-            }
-        } );
-        minus.setOnClickListener( new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                int temp = Integer.parseInt( tempvalue );
-                if (temp > 16)
-                    --temp;
-                tempd.setValue( "" + temp );
-            }
-        } );
+                    } else {
+                        power.setTextColor( getResources().getColor( android.R.color.black ) );
+                    }
+                }
 
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            } );
+
+            power.setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (powervalue.equals( "1" )) {
+                        power.setTextColor( getResources().getColor( android.R.color.black ) );
+                        powerd.setValue( "0" );
+                    } else {
+                        powerd.setValue( "1" );
+                        power.setTextColor( getResources().getColor( android.R.color.holo_green_light ) );
+
+                    }
+                }
+            } );
+            tempd.addValueEventListener( new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    tempvalue = (String) dataSnapshot.getValue();
+                    temperature.setText( tempvalue + " °C" );
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            } );
+            plus.setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int temp = Integer.parseInt( tempvalue );
+                    if (temp < 40)
+                        ++temp;
+                    tempd.setValue( "" + temp );
+                }
+            } );
+            minus.setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int temp = Integer.parseInt( tempvalue );
+                    if (temp > 16)
+                        --temp;
+                    tempd.setValue( "" + temp );
+                }
+            } );
+        }
+        else
+            Toast.makeText( this, "Check your internet connection", Toast.LENGTH_LONG ).show();
     }
-
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService( Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
     @Override
     protected void onResume() {
         temperature = findViewById( R.id.temp );
