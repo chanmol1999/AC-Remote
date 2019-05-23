68b30ce76339a4f0736296f44159ae5900c1b2c9
chanmol1999 <chanmol1999@gmail.com>
2019-05-23 15:07:03 +0530

    Initialised app

---
+++ AC--Remote/app/src/main/java/com/example/ac_remote/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.ac_remote;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_main );
+    }
+}
